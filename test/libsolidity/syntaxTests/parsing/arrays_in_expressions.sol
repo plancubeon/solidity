1b13e07ab3417206388b4b7bdf167da1e06fa6f0
Daniel Kirchner <daniel@ekpyron.org>
2018-07-03 15:10:39 +0200

    Update tests.

--- test/libsolidity/syntaxTests/parsing/arrays_in_expressions.sol
+++ test/libsolidity/syntaxTests/parsing/arrays_in_expressions.sol
@@ -5,4 +5,4 @@ contract c {
 // Warning: (32-39): Variable is declared as a storage pointer. Use an explicit "storage" keyword to silence this warning.
 // Warning: (45-60): Variable is declared as a storage pointer. Use an explicit "storage" keyword to silence this warning.
 // TypeError: (32-43): Type int_const 7 is not implicitly convertible to expected type contract c[10] storage pointer.
-// Warning: (45-60): Uninitialized storage pointer. Did you mean '<type> memory x'?
+// DeclarationError: (45-60): Uninitialized storage pointer. Did you mean '<type> memory x'?
